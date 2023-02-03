3c196dbbb5899238dc8decb18032c9006a9d55f3
m4nu3l-00 <61203017+m4nu3l-00@users.noreply.github.com>
2023-02-03 20:22:38 +0100

    added hohmann transfer

--- Code/dgl.m
+++ Code/dgl.m
@@ -1,21 +1,25 @@
-global m0 dm v_gas G M  brennschluss deltaPhi m0_min deltaTBrennschluss crashed radiusMoon startAngle
-dm      = 20;   %Gewichtsabnahme pro Zeitschritt
-m0 = 3500;%3700; %Anfangsgewicht um auf 3,5k m/s zu kommen ca. 800-900 
-m0_min  = 1000;  %Endgewicht
-v0=0;           %Anfangsgeschwindigkeit
-v_gas   = 2000;             %Austrittsgeschwindigkeit des Gases
-G       = 6.67430*10^-11;   %Gravitationskonstante
-M       = 7.3483*10^22;     %Mondmasse
-h       = 1737.4*10^3;      %Starthöhe, entspricht Radius des Mondes, cant be global or it will not work anymore
+global m0 dm v_gas G M  brennschluss deltaPhi m0_min deltaTBrennschluss  startAngle heights flag svd_t extra_boost matlab_fix hohmann_const
+dm      = 20;   % Weight loss per time step
+m0 = 3500;      % Initial weight
+m0_min  = 1000; % Final weight
+v0=0;           % Initial speed
+v_gas   = 2000;             %Outlet velocity of the gas
+G       = 6.67430*10^-11;   %Gravitational constant
+M       = 7.3483*10^22;     %Lunar mass
+h       = 1737.4*10^3;      %Start altitude, corresponds to radius of the moon
 radiusMoon = 1737.4*10^3;
-startAngle = pi/4;
+startAngle = pi/4;          
+flag = 1;                   % extremum already found
+svd_t = 0;                  % saved time if extremum found
+hohmann_const = 1;          % speed of the rocket at extremum
+matlab_fix = 0;
+extra_boost = 0;
+heights=[h h;
+         0 0;
+         0 0];
 
-
-orbitHeight = 400000;%400km height of orbit
-vOrbit = sqrt(G*M/orbitHeight);%velocity to stay in orbit
-
-brennschluss = ceil((m0-m0_min) / dm);
-tspan= 0:0.1:(brennschluss+40000);
+brennschluss = ceil((m0-m0_min) / dm) - hohmann_const;
+tspan= 0:0.1:(brennschluss+100000);
 startPosX = 0;
 startPosY = h;
 startVX = 0;
@@ -24,109 +28,70 @@ startVY = 0;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %for loop for tessting and plotting different scenarios
 
-for i=18:2:22
+for i=18:2:18
     deltaTBrennschluss = i;
     endAngle = -pi/4-0.1;
     figure
     hold on
     th = 0:pi/50:2*pi;
     xunit = radiusMoon * cos(th);
-    %xUnit2 = (orbitHeight + radiusMoon) * cos(th);
     yunit = radiusMoon * sin(th);
-    %yUnit2 = (orbitHeight + radiusMoon)* sin(th);
     plot(xunit, yunit);
-   % plot(xUnit2,yUnit2);
-%     plot(sX2,sY2);
-%     plot(sX3,sY3);
-    %axis([-4*10^6 4*10^6 -5*10^6 5*10^6])
-    for j=(startAngle - endAngle)/(brennschluss-deltaTBrennschluss)*0.9:(startAngle - endAngle)/(brennschluss-deltaTBrennschluss)*0.1:(startAngle - endAngle)/(brennschluss-deltaTBrennschluss)*1.1
-        crashed = false;
-        deltaPhi = j;
+    deltaPhi=(startAngle - endAngle)/(brennschluss-deltaTBrennschluss)*0.9; % angle which acceleration changes over time
+    flag = 1;
+        svd_t = 0;
+        extra_boost = 0;
+        matlab_quickfix = 0;
         [tspan,pos1]=ode45(@bdgl, tspan, [startPosX;startVX;startPosY;startVY]);
-        disp(crashed+", "+deltaPhi+", "+deltaTBrennschluss);
-        %     crashed = false;
-        %     endAngle = -pi/4-0.1;
-        %     deltaPhi = (startAngle - endAngle)/(brennschluss-deltaTBrennschluss);
-        %     [tspan,pos2]=ode45(@bdgl, tspan, [startPosX;startVX;startPosY;startVY]);
-        %     disp(crashed+", "+deltaPhi+", "+deltaTBrennschluss);
-        %     crashed = false;
-        %     endAngle = -pi/4+0.1;
-        %     deltaPhi = (startAngle - endAngle)/(brennschluss-deltaTBrennschluss);
-        %     [tspan,pos3]=ode45(@bdgl, tspan, [startPosX;startVX;startPosY;startVY]);
-        %     disp(crashed+", "+deltaPhi+", "+deltaTBrennschluss);
         
         sX1 = pos1(:,1);
         sY1 = pos1(:,3);
         vX1 = pos1(:,2);
         vY1 = pos1(:,4);
         plot(sX1,sY1);
-        %%for plot over time not instantly
-%         for k=1:length(sX1)
-%             plot(sX1(1:k),sY1(1:k));
-%             pause(0.000000001)
-%         end
-    end
     axis([-5.5*10^6 5.5*10^6 -5.5*10^6 5.5*10^6])
     title('moon, destinated orbit and rocket trajectory');
     hold off
-    legend('moon','rocket trajectory1','rocket trajectory2','rocket trajectory3');
-%     sX2 = pos2(:,1);
-%     sY2 = pos2(:,3);
-%     vX2 = pos2(:,2);
-%     vY2 = pos2(:,4);
-%     sX3 = pos3(:,1);
-%     sY3 = pos3(:,3);
-%     vX3 = pos3(:,2);
-%     vY3 = pos3(:,4);
+    legend('moon','rocket trajectory1');
 
-    %plot v
-%     figure
-%     hold on
-%     plot(tspan,sqrt(vX1.^2+vY1.^2));
-%     plot(tspan,sqrt(vX2.^2+vY2.^2));
-%     plot(tspan,sqrt(vX3.^2+vY3.^2));
-%     title('v');
-%     hold off
-%     legend('v1','v2','v3');
-
-    %plot moon, orbit and trajectory
-%     figure
-%     hold on
-%     th = 0:pi/50:2*pi;
-%     xunit = h * cos(th);
-%     xUnit2 = (orbitHeight + h) * cos(th);
-%     yunit = h * sin(th);
-%     yUnit2 = (orbitHeight +h)* sin(th);
-%     plot(xunit, yunit);
-%     plot(xUnit2,yUnit2);
-%     plot(sX1,sY1);
-%     plot(sX2,sY2);
-%     plot(sX3,sY3);
-%     %axis([-4*10^6 4*10^6 -5*10^6 5*10^6])
-%     title('moon, destinated orbit and rocket trajectory');
-%     hold off
-%     legend('moon','destinated orbit','rocket trajectory1','rocket trajectory2','rocket trajectory3');
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %y(1):sX  y(2):vX  y(3):sY  y(4):vY
 function [fx] = bdgl(t,y)
-global m0 dm v_gas G M  brennschluss deltaPhi m0_min deltaTBrennschluss crashed radiusMoon startAngle
-%h       = 1737.4*10^3;      %start height, radius of moon
-%h = h+sqrt(y(2)^2+y(4)^2)*t;%calculate height, only for start because not vektorial at the moment
-h = sqrt(y(1)^2+y(3)^2);
-if(h < radiusMoon)
-    crashed = true;
-    %disp(h);
+global m0 dm v_gas G M  brennschluss deltaPhi m0_min deltaTBrennschluss startAngle heights flag svd_t extra_boost matlab_fix hohmann_const
+h = sqrt(y(1)^2+y(3)^2); %calculate height, only for start because not vektorial at the moment
+
+% start hohmann manover
+if(heights(1,1) < heights(1,2)&&  heights(1,2) > h && flag && y(1)< -2*10e6 && y(3) < 0)
+    flag = 0;
+    extra_boost = 1;
+    svd_t = t;
 end
+
+% log old positions and heights
+heights(1,1) = heights(1,2);
+heights(2,1) = heights(2,2);
+heights(3,1) = heights(3,2);
+heights(1,2) = h;
+heights(2,2) = y(1);
+heights(3,2) = y(3);
+
 if(t < brennschluss)
     m = m0 - dm*t;                  %calculate the rocket mass over time
 else
-    m = m0_min; %if brennschlusss then no more loss of mass
+    if(svd_t == 0)
+        m = m0 - dm*brennschluss; %if brennschlusss then no more loss of mass
+    else
+        if((svd_t+hohmann_const)<t) % time slot for hohmann manover
+            m = m0 - dm*(brennschluss+t-svd_t);
+        else
+            m = m0_min;
+        end
+    end
 end
 
-%if(t < deltaTBrennschluss || ((t > deltaTBrennschluss + 5) && (t < brennschluss + 5)))
 if(t < brennschluss)
     if(t < deltaTBrennschluss)%until deltaTBrennschluss just start without turning
         aRakete = (dm*v_gas)/(m0-dm*t)*[0;1]; %acceleration of rocket vektorial for start
@@ -135,16 +100,26 @@ if(t < brennschluss)
         %will turn, pi/2 is in y direction, pi/4 is middle between y and x
         %axis so 45 degree
         aRakete = (dm*v_gas)/(m0-dm*t)*[cos(startAngle-deltaPhi*(t-deltaTBrennschluss));sin(startAngle-deltaPhi*(t-deltaTBrennschluss))]; %acceleration of rocket vektorial for curve
-        %disp(pi/2-deltaPhi*t);
     end
 else
-    aRakete = [0;0];
+    % only extra speed at extremum
+    if(extra_boost == 1)
+        % matlab jumps backwards with the time --> FIX MATLAB
+        if(m == m0_min && t > matlab_fix)
+            x_coord = heights(2,2)-heights(2,1); % calculate x
+            y_coord = heights(3,2)-heights(3,1); % calcualte y
+            coord_angle = angle(x_coord + 1i *y_coord); % calculated angle for hohman manover
+            aRakete = (dm*v_gas)/(m0-dm*t)*[cos(coord_angle);sin(coord_angle)];
+        else
+            aRakete = [0;0];
+        end
+    else
+        aRakete = [0;0];
+    end
 end
+matlab_fix = t;
 aG = G*(M+m)/h^2 * [-y(1)/h;-y(3)/h];%gravitation acceleration of moon vektorial
 a = aRakete + aG ; %combined acceleration vektorial
-% if(t < 5000)
-%      fprintf('t: %2.4f and h: %4.4f\n',t,h);
-% end
 dxdt = [y(2);a(1)];%vX,aX
 dydt = [y(4);a(2)];%vY,aY
 fx = [dxdt;dydt];
